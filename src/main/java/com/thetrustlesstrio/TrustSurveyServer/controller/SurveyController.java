6dc2022c181e06c1e487fc23958624669f0dd0bc
Jaeyub Song <jaeyub.song@kaist.ac.kr>
2023-05-10 04:26:32 +0900

    Removed unnecessary imports

--- src/main/java/com/thetrustlesstrio/TrustSurveyServer/controller/SurveyController.java
+++ src/main/java/com/thetrustlesstrio/TrustSurveyServer/controller/SurveyController.java
@@ -5,9 +5,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 

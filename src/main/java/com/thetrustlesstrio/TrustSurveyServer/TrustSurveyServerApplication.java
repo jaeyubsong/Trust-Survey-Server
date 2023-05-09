3c9b4e360ecfae9efbf0da0892d9757257a22f06
Dongpyeong Seo <bludrimjr@gmail.com>
2023-05-09 14:51:52 +0900

    Resolve CORS issue temporarily

--- src/main/java/com/thetrustlesstrio/TrustSurveyServer/TrustSurveyServerApplication.java
+++ src/main/java/com/thetrustlesstrio/TrustSurveyServer/TrustSurveyServerApplication.java
@@ -3,6 +3,9 @@ package com.thetrustlesstrio.TrustSurveyServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // 나중에 Database 쓰기 위해서는 얘 하기 넣어야 함
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
@@ -12,4 +15,13 @@ public class TrustSurveyServerApplication {
 		SpringApplication.run(TrustSurveyServerApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
 }
